30ecbac3cbe6b3505a75c27eb654822e10b74888
P1pega <146541895+P1pega@users.noreply.github.com>
2023-09-30 19:30:38 +0300

    Add files via upload

---
+++ Lab1/C#3/C#3/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите трезначное число");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int sum = a / 100 + (a % 100) / 10 + a % 10;
+        int product = (a / 100) * ((a % 100) / 10) * (a % 10);
+        Console.WriteLine($"Сумма цифр: {sum}");
+        Console.WriteLine($"Произведение цифр: {product}");
+
+    }
+}
