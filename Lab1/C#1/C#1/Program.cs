f56b4bdac345ffca62fd340c94bab42fbe24722a
P1pega <146541895+P1pega@users.noreply.github.com>
2023-09-30 15:03:30 +0300

    Add files via upload

---
+++ Lab1/C#1/C#1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите радиус");
+        double r = Convert.ToInt32(Console.ReadLine());
+        double l = 2 * Math.PI * r;
+        double S = Math.PI * Math.Pow(r, 2);
+        double V = (4 / 3.0) * Math.PI * Math.Pow(r, 3);
+        Console.WriteLine($"Окружность: {l}");
+        Console.WriteLine($"Площадь: {S}");
+        Console.WriteLine($"Объем сферы: {V}");
+    }
+}
