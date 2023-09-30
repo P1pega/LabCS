c8ff239cd12ffaca5a2679edd79988516bae39fa
P1pega <146541895+P1pega@users.noreply.github.com>
2023-09-30 15:05:24 +0300

    Add files via upload

---
+++ Lab1/C#1/C#2/C#2/Program.cs
@@ -0,0 +1,16 @@
+﻿private static void Main(string[] args)
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Перед заменой:");
+    Console.WriteLine("a=" + a);
+    Console.WriteLine("b=" + b);
+    int temp = a;
+    a = b;
+    b = temp;
+    Console.WriteLine("После замены:");
+    Console.WriteLine("a=" + a);
+    Console.WriteLine("b=" + b);
+
+}
+}
---
+++ Lab1/C#1/C#4/C#4/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите трехзначное число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int dig1 = a / 100;
+        int dig2 = a / 10 % 10;
+        int dig3 = a % 10;
+        int swap = dig3 * 100 + dig2 * 10 + dig1;
+        Console.WriteLine("Заменненый номер: " + swap);
+    }
+}
