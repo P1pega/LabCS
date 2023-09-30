c2a2039f27e477af89db78984ea9e5afce785d75
P1pega <146541895+P1pega@users.noreply.github.com>
2023-09-30 19:31:58 +0300

    Add files via upload

---
+++ Lab1/C#5/C#5/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите длину катетов: ");
+        double a = Convert.ToInt32(Console.ReadLine());
+        double b = Convert.ToInt32(Console.ReadLine());
+        double P = ((Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2))) + (a + b));
+        double S = (a * b / 2);
+        Console.WriteLine($"Периметр: {P}");
+        Console.WriteLine($"Площадь: {S}");
+    }
+}
